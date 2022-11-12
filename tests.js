1b0dde7d9185594ce5a2c786c845eaf5443598fa
Sablinita <sablinahare@gmail.com>
2022-11-12 17:42:39 +0100

    done one more

--- tests.js
+++ tests.js
@@ -71,6 +71,7 @@ describe("Ejemplos resueltos: Operaciones iterables", () => {
     it("map nos permite realizar operaciones elemento a elemento", () => {
 
         let list = [1,5,7,9,11,13];
+        let newArr = list.map(multiplicarPor2);
 
         function multiplicarPor2(num) {
             return num * 2;
@@ -159,7 +160,7 @@ describe("Operaciones iterables", () => {
         let list = [1,5,7,9,11,13];
 
         // utiliza la función map para aplicar los números de la lista
-        list = "???";
+        
         
 
         expect(list).to.have.same.members([0,4,6,8,10,12]);
@@ -183,7 +184,7 @@ describe("Operaciones iterables", () => {
         let knownExoplanets = [ "TOI-1298 b","TOI-132 b","TOI-1333 b","TOI-1338 b","TOI-1431 b","TOI-1444 b","TOI-1478 b","TOI-150.01","TOI-157 b","TOI-1601 b","TOI-163 b","TOI-1634 b","TOI-1685 b","TOI-169 b","TOI-172 b","TOI-1728 b","TOI-1749 b","TOI-1749 c"];
 
         // utiliza la función includes para comprobar si la lista de planetas contiene el planeta TOI-1634 b
-        let result = "???";
+        let result = knownExoplanets.includes("TOI-1634 b");
         
         expect(result).to.equal(true);        
     })
@@ -195,8 +196,7 @@ describe("Operaciones iterables", () => {
         // utiliza la función filter para quedarnos con los planetas que terminan por la letra c
         // puedes utilizar la función .endsWith para comprobar si un string termina por una letra
         // p.ej. "Hola".endsWith('a') devuelve true
-        let result = "???";
-        
+       
 
         // todos los planetas de result terminan con la letra c
         expect(result.every( p => p.endsWith('c'))).to.equal(true);        
